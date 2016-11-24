e715d58e87169237d5ebb79bd542a98b5086a0ed
Angle-yc <916374683@qq.com>
2016-11-24 21:48:32 +0800

    流式布局

--- app/src/main/java/com/angle/biaoge/MyGroupView.java
+++ app/src/main/java/com/angle/biaoge/MyGroupView.java
@@ -2,7 +2,9 @@ package com.angle.biaoge;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * Created by Administrator on 2016/11/24 0024.
@@ -23,6 +25,24 @@ public class MyGroupView extends ViewGroup{
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        int maxW=getResources().getDisplayMetrics().widthPixels-getPaddingLeft()-getPaddingRight();
+        int lineCount=0;
+        int sumW=0;
+        int i=0;
+        for (i=0;i<getChildCount();i++){
+            View view=getChildAt(i);
+            view.measure(0,0);
+            int w=view.getMeasuredWidth();
+            int h=view.getMeasuredHeight();
+            if(sumW<=maxW&&i%4!=0){
+                view.layout(sumW,lineCount*h,sumW+w,lineCount*h+h);
+                sumW+=w+10;
+            }else{
+                sumW=0;
+                lineCount++;
+                view.layout(sumW,lineCount*h,sumW+w,lineCount*h+h);
+                sumW+=w+10;
+            }
+        }
     }
 }
